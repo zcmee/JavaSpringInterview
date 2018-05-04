d2b72d957c72ddaaee30d8cbcb70f62c7452f530
adam.bryksy <adam.bryksy@ramsat.pl>
2018-05-04 12:15:02 +0200

    Empty Java&Spring Project

---
+++ src/main/java/com/github/zcmmee/SpringJavaInterview/SpringJavaInterviewApplication.java
@@ -0,0 +1,12 @@
+package com.github.zcmmee.SpringJavaInterview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJavaInterviewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringJavaInterviewApplication.class, args);
+	}
+}
---
+++ src/test/java/com/github/zcmmee/SpringJavaInterview/SpringJavaInterviewApplicationTests.java
@@ -0,0 +1,16 @@
+package com.github.zcmmee.SpringJavaInterview;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringJavaInterviewApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
